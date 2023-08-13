7951cbfc73f03e12951a1864221f359d8be5fc79
ashwini-sy <ashwinisy1995@gmail.com>
2023-08-13 12:56:00 +0530

    Added Add Method

--- demo.java
+++ demo.java
@@ -2,7 +2,19 @@ public class  demo{
 
  public static void main(String[] arg)  
  { 
-     System.out.println("Demo Session");
+     System.out.println("Addition of Two Number: ");
+     int i=10,j=2;
+System.out.println(add(i,j));
+   
+
+
+ }
+
+ public static int add(int a,int b){
+
+    int sum=a+b;
+
+    return sum;
  }
 
 }
